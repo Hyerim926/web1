bf2a180cb6a885ad7beb50d9290082ebd922863b
Hyerim926 <82012938+Hyerim926@users.noreply.github.com>
2021-09-20 02:04:04 +0900

    Add files via upload
    script 보완

---
+++ jsStudy/main/js/script.js
@@ -0,0 +1,40 @@
+// 다크모드handler 함수
+var Links = {
+    setColor:function (color){
+    //   var alist = document.querySelectorAll('a');
+    //   var i = 0;
+    //   while(i < alist.length){
+    //     alist[i].style.color = color;
+    //     i++;
+    //   }
+    $('a').css('color', color);
+    }
+  }
+  var Body = {
+    setColor:function(color){
+    // document.querySelector('body').style.color = color;
+    $('body').css('color', color);
+},
+setBackgroundColor:function(color){
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
+  }
+  }
+  
+  function nightDayHandler(self){
+      var target = document.querySelector('body');
+  if(self.value == 'night'){
+      Body.setBackgroundColor('#000'); 
+      Body.setColor('#fff');
+      self.value = 'day';
+  
+      Links.setColor('powderblue');
+  
+  } else {
+    Body.setBackgroundColor('#fff');
+    Body.setColor('#000');
+    self.value = 'night'
+    
+    Links.setColor('blue');
+  }
+  }
